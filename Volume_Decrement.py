3e86bf6910a406153733c6b62ea721e833de7e52
wxhts <david.itenberg@wuxiapptec.com>
2016-04-22 15:02:19 -0400

    Improved run time
    Improved run time of script by dynamically building a string with all compound IDs to pass into query via IN clause. This reduces thousands of accesses of the DB per execution.

--- Volume_Decrement.py
+++ Volume_Decrement.py
@@ -1,16 +1,28 @@
 import pandas as pd
 import sqlite3
 
-conn = sqlite3.connect('C:\Users\IVtB Lab\Desktop\HTSCompounds1.db')
+def stringify(identifier):
+    return "'{}'".format(identifier)
+
+conn = sqlite3.connect('C:\Users\IVtB Lab\Desktop\HTSCompounds.db')
 c = conn.cursor()
 
 input_path = raw_input('Enter the PATH of compound file: ')
 
 df = pd.read_csv(input_path)
-
 cmpd = df['CompoundID']
 
-for i in cmpd:
-    c.execute('UPDATE EchoSource SET Volume = Volume - 1.5 WHERE Client_ID = ?', (i,))
+count = 0
+query = ''
+for x in cmpd:
+    count += 1
+    if count == 1:
+        query = '(' + stringify(x) + ','
+    elif count == len(cmpd):
+        query = query + stringify(x) + ')'
+    else:
+        query = query + stringify(x) + ','
 
+c.execute('UPDATE EchoSource SET Volume = Volume - 1.5 WHERE Client_ID IN ' + query)
 conn.commit()
+
