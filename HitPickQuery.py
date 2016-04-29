e785475c1558976da45a401284b6eef1201b8147
wxhts <wxhts@users.noreply.github.com>
2016-04-29 10:03:31 -0400

    Added structure conditional

--- HitPickQuery.py
+++ HitPickQuery.py
@@ -2,14 +2,16 @@ import pandas as pd
 import sqlite3
 import csv
 
+
 def stringify(identifier):
     return "'{}'".format(identifier)
+
 
 def build_query(dataframe):
     query = ''
     count = 0
     for x in dataframe:
-        count += 1 
+        count += 1
         if count == 1:
             query = '(' + stringify(x) + ','
         elif count == len(dataframe):
@@ -18,13 +20,14 @@ def build_query(dataframe):
             query = query + stringify(x) + ','
     return query
 
+
 conn = sqlite3.connect('C:\Users\IVtB Lab\Desktop\HTSCompounds.db')
 conn.text_factory = str
 c = conn.cursor()
 
 hit_file = raw_input('Enter PATH for hit file: ')
 output_path = raw_input('Enter PATH for output file: ')
-structures = raw_input('Are structures need? (Y/N)' )
+structures = raw_input('Are structures need? (Y/N) ')
 
 with open(output_path, 'wb') as output_file:
     csvwriter = csv.writer(output_file)
@@ -32,11 +35,11 @@ with open(output_path, 'wb') as output_file:
     df = pd.read_csv(hit_file)
     compounds = df['CompoundID'].dropna()
     cmpd = build_query(compounds)
-	
-	if structures == 'Y':
-		findcompound = c.execute("SELECT EchoSource.Barcode, EchoSource.Well, EchoSource.Client_ID, ChinaSource.SMILES FROM EchoSource INNER JOIN ChinaSource ON EchoSource.Client_ID=ChinaSource.Client_ID WHERE EchoSource.Client_ID IN " + cmpd)
-		csvwriter.writerows(findcompound.fetchall())
-	else:
-		findcompound = c.execute("SELECT Barcode, Well, Client_ID FROM EchoSource WHERE Client_ID IN " + cmpd)
-		csvwriter.writerows(findcompound.fetchall())
 
+    if structures == 'Y':
+        findcompound = c.execute(
+            "SELECT EchoSource.Barcode, EchoSource.Well, EchoSource.Client_ID, ChinaSource.SMILES FROM EchoSource INNER JOIN ChinaSource ON EchoSource.Client_ID=ChinaSource.Client_ID WHERE EchoSource.Client_ID IN " + cmpd)
+        csvwriter.writerows(findcompound.fetchall())
+    else:
+        findcompound = c.execute("SELECT Barcode, Well, Client_ID FROM EchoSource WHERE Client_ID IN " + cmpd)
+        csvwriter.writerows(findcompound.fetchall())
