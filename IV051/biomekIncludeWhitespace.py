7f063567921250f74985616b6c1420d28b2505c6
wxhts <wxhts@users.noreply.github.com>
2016-10-17 16:08:09 -0400

    Create biomekIncludeWhitespace.py

---
+++ IV051/biomekIncludeWhitespace.py
@@ -0,0 +1,22 @@
+from itertools import product
+import pandas as pd
+from createWellIndex import createWellIndex
+
+filesource = raw_input('Enter the PATH to export file: ')
+fileout = raw_input('Enter the PATH for output file:')
+plate = createWellIndex(filesource)
+
+boundx = range(1, 17)
+boundy = range(3, 23)
+barcodes = set(plate['Barcode'])
+sampleRegion = []
+
+for barcode in barcodes:
+    for x, y in product(boundx, boundy):
+        samples = plate[(plate['Barcode'] == barcode) & (plate['Row'] == x) & (plate['Column'] == y)]
+        sampleRegion.append(samples)
+
+samplesOnly = pd.concat(sampleRegion)
+sortedSamples = samplesOnly.sort_values(by=['Name', 'Row', 'Column'])
+
+sortedSamples.to_csv(fileout, index=False)
