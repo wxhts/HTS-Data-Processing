615665f412f1bd6cbcfd66ea3c56ec5a4a0f67f0
wxhts <wxhts@users.noreply.github.com>
2016-10-19 13:05:29 -0400

    output file name as 1536plate

--- IV051/biomekIncludeWhitespace.py
+++ IV051/biomekIncludeWhitespace.py
@@ -2,21 +2,26 @@ from itertools import product
 import pandas as pd
 from createWellIndexBiomek import createWellIndexBiomek
 
+
 filesource = raw_input('Enter the PATH to export file: ')
-fileout = raw_input('Enter the PATH for output file:')
+fileout = raw_input('Enter the PATH for output directory: ')
 plate = createWellIndexBiomek(filesource)
 
 boundx = range(1, 17)
 boundy = range(3, 23)
-barcodes = set(plate['Barcode'])
-sampleRegion = []
+barcodes = sorted(list(set(plate['Barcode'])))
+namefile = barcodes[-1]
+barcode_iter = barcodes[:-1]
 
-for barcode in barcodes:
+sampleRegion = []
+for barcode in barcode_iter:
     for x, y in product(boundx, boundy):
         samples = plate[(plate['Barcode'] == barcode) & (plate['Row'] == x) & (plate['Column'] == y)]
         sampleRegion.append(samples)
 
 samplesOnly = pd.concat(sampleRegion)
+samplesOnly['Concentration'] = 7.5
+SamplesOnly['1536Plate'] = namefile
 sortedSamples = samplesOnly.sort_values(by=['Name', 'Row', 'Column'])
 
-sortedSamples.to_csv(fileout, index=False)
+sortedSamples.to_csv(fileout + '/' + namefile + '.csv', index=False)
